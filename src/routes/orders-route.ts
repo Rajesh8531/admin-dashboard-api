e4a883e48873dec871e3e623a0b45c7e947dad79
Rajesh <rajesh99ed@gmail.com>
2024-07-09 19:12:04 +0530

    final

--- src/routes/orders-route.ts
+++ src/routes/orders-route.ts
@@ -1,17 +1,8 @@
 import express from 'express'
-import { auth } from '../middlewares/auth'
 import { getOrders } from '../controllers/orders-controller'
 
 const router = express.Router({mergeParams:true})
 
 router.get('/',getOrders)
-
-// router.get('/:productId',getProduct)
-
-// router.post('/',auth,createProduct)
-
-// router.patch('/:productId',auth,updateProduct)
-
-// router.delete('/:productId',auth,deleteProduct)
 
 export default router
